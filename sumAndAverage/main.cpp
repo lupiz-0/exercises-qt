495a62eab6afa542ab9d4fd00f0ac45199593ec3
Luigi Pizzitola <lupiz@develer.com>
2020-10-20 14:57:50 +0200

    Finito programma a cui forniti dei valori numerici restituisce la loro somma e la loro media.

--- sumAndAverage/main.cpp
+++ sumAndAverage/main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
+#include <string>
 
-using namespace std;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    cout << "Insert some numeric values. Insert \"end\" to execute sum and average." << endl;
+    int count = 0;
+    float sum = 0;
+    for(;;)
+    {
+        cout << "insert: ";
+        string stringInserted;
+        cin >> stringInserted;
+        if(stringInserted.compare("end") == 0)
+            break;
+        sum += std::stof(stringInserted);
+        count++;
+    }
+
+    cout << "sum: " << sum << " " << "media: " << sum/count << endl;
+
     return 0;
 }
