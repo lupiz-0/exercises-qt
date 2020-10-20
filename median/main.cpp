d13db05bd43da60dedcd3e8af61ed6a72f2f9c2e
Luigi Pizzitola <lupiz@develer.com>
2020-10-20 16:24:03 +0200

    exercise of calculation of median completed

--- median/main.cpp
+++ median/main.cpp
@@ -1,9 +1,47 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
 
-using namespace std;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::vector;
+
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    cout << "Insert some numeric values. Push enter without enter the value to  calculate median." << endl;
+    vector<float> values;
+    for(;;)
+    {
+        cout << "insert: ";
+        string stringInserted;
+        getline(std::cin, stringInserted);
+        if(stringInserted.size() == 0)
+            break;
+        values.push_back(std::stof(stringInserted));
+    }
+
+    std::sort (values.begin(), values.end());
+
+    float median = 0;
+    if(values.size() % 2 == 0) {
+        int index0 = values.size()/2;
+        int index1 = values.size()/2 - 1;
+        median = (values[index0] + values[index1])/2;
+    }
+    else {
+        int index = (values.size() - 1)/2;
+        median = values[index];
+    }
+
+    cout << "the median of the values: ";
+    for(ulong i = 0; i < values.size(); i++){
+        cout << values[i] << " ";
+    }
+    cout << "   is: " << median << endl;
+
     return 0;
 }
