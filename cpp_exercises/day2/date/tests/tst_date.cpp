31c503d154675f74dcbb03964698ac26b9b0d637
Luigi Pizzitola <lupiz@develer.com>
2020-10-26 11:01:03 +0100

    Controllo di validia dei dati inseriti nel costruttore non di default e modifica del relativo test

--- cpp_exercises/day2/date/tests/tst_date.cpp
+++ cpp_exercises/day2/date/tests/tst_date.cpp
@@ -49,10 +49,45 @@ void testDate::defaultConstructorTest() {
 }
 
 void testDate::notDefaultConstructorTest() {
-    Date date(23, Date::October, 2020);
-    QCOMPARE(date.day(), 23);
-    QCOMPARE(date.month(), Date::October);
-    QCOMPARE(date.year(), 2020);
+    Date date0(26, Date::October, 2020);
+    QVERIFY(date0.compare(26, Date::October, 2020));
+
+    Date date1(26, Date::October, -1);
+    QVERIFY(isDefaultDate(date1));
+
+    Date date2(26, Date::October, 0);
+    QVERIFY(date2.compare(26, Date::October, 0));
+
+    Date date3(26, 0, 2020);
+    QVERIFY(isDefaultDate(date3));
+
+    Date date4(26, 1, 2020);
+    QVERIFY(date4.compare(26, 1, 2020));
+
+    Date date5(26, 13, 2020);
+    QVERIFY(isDefaultDate(date5));
+
+    Date date6(26, 12, 2020);
+    QVERIFY(date6.compare(26, 12, 2020));
+
+    Date date7(1, Date::November, 2020);
+    QVERIFY(date7.compare(1, Date::November, 2020));
+
+    Date date8(0, Date::November, 2020);
+    QVERIFY(isDefaultDate(date8));
+
+    Date date9(30, Date::November, 2020);
+    QVERIFY(date9.compare(30, Date::November, 2020));
+
+    Date date10(31, Date::November, 2020);
+    QVERIFY(isDefaultDate(date10));
+
+    Date date11(29, Date::February, 2019);
+    QVERIFY(isDefaultDate(date11));
+
+    Date date12(29, Date::February, 2020);
+    QVERIFY(date12.compare(29, Date::February, 2020));
+
 }
 
 void testDate::insertionOperatorTest() {
